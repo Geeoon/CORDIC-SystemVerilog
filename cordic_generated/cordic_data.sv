92a123d0d9d50d1e1b6ece73609def494e3a0d9a
Geeoon Chung <geeoon13@gmail.com>
2025-09-05 17:47:01 -0500

    Using signed values

--- cordic_generated/cordic_data.sv
+++ cordic_generated/cordic_data.sv
@@ -20,27 +20,25 @@ module cordic_data
     output logic [BIT_WIDTH-1:0] x, y;
     output logic reached_target, dir; 
 
-    logic [BIT_WIDTH:0] x_reg, y_reg;  // internal register with an extra bit due to pre-scaling
+    // internal register with two extra bits due to pre-scaling and signedness
+    logic signed [BIT_WIDTH+1:0] x_reg, y_reg, shifted_x, shifted_y;
 
     logic [BIT_WIDTH-1:0] target_reg, current;
     logic [BIT_WIDTH-1:0] diff;  // technically if we make this -1 instead of -2, it will allow us to have
     // bit widths of 1, but at that point, you could just write a normal LUT
-    logic [LOG_2_BIT_WIDTH:0] i;  // we need an extra bit to bitshift to the end; TODO: rename
-
-    assign x = x_reg[BIT_WIDTH-1:0];
-    assign y = y_reg[BIT_WIDTH-1:0];
+    logic [LOG_2_BIT_WIDTH-1:0] i;
 
     always_ff @(posedge clk) begin
         if (add) begin
             current <= current + diff;
-            x_reg <= x_reg - (y_reg >> i);
-            y_reg <= y_reg + (x_reg >> i);
+            x_reg <= x_reg - shifted_y;
+            y_reg <= y_reg + shifted_x;
         end
 
         if (sub) begin
             current <= current - diff;
-            x_reg <= x_reg + (y_reg >> i);
-            y_reg <= y_reg - (x_reg >> i);
+            x_reg <= x_reg + shifted_y;
+            y_reg <= y_reg - shifted_x;
         end
 
         if (iter) begin
@@ -49,18 +47,39 @@ module cordic_data
         end
 
         if (load_regs) begin
-            diff <= {1'b1, {(BIT_WIDTH-1){1'b0}}};  // NOTE: might give truncation warning
+            diff <= {1'b1, {(BIT_WIDTH-1){1'b0}}};
             current <= 0;
             target_reg <= target;
             x_reg <= K;
             y_reg <= 0;
-            i <= 1;
+            i <= 0;
         end
     end  // always_ff
 
     always_comb begin
-        reached_target = i == {1'b1, {(LOG_2_BIT_WIDTH){1'b0}}};
+        reached_target = i == {(LOG_2_BIT_WIDTH){1'b1}};
         dir = current < target;
+        if (x_reg[BIT_WIDTH+1]) begin
+            // if its negative
+            // clip off the extra bit then convert to magnitude
+            x = ~x_reg[BIT_WIDTH-1:0] + {{(BIT_WIDTH-1){1'b0}}, 1'b1};
+            shifted_x = x_reg >>> i;
+        end else begin
+            // if its positive
+            shifted_x = x_reg >> i;
+            x = x_reg[BIT_WIDTH-1:0];
+        end
+
+        if (y_reg[BIT_WIDTH+1]) begin
+            // if its negative
+            // clip off the extra bit then convert to magnitude
+            y = ~y_reg[BIT_WIDTH-1:0] + {{(BIT_WIDTH-1){1'b0}}, 1'b1};
+            shifted_y = y_reg >>> i;
+        end else begin
+            // if its positive
+            y = y_reg[BIT_WIDTH-1:0];
+            shifted_y = y_reg >> i;
+        end 
     end  // always_comb
 
 endmodule  // cordic_data
